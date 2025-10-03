70697b81efc2ae6bce157b7245e2efb80f6de438
Hareendra sri sai <maddipatihareendra@gmail.com>
2025-10-03 12:36:39 +0530

    connected MySQL

--- database.py
+++ database.py
@@ -10,15 +10,18 @@ from urllib.parse import quote_plus
 load_dotenv()
 
 # Read components
-DB_USER = os.getenv("DB_USER")
+# DB_USER = os.getenv("DB_USER")
 DB_PASS = quote_plus(os.getenv("DB_PASS")) 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_NAME = os.getenv("DB_NAME")
 
 # Build SQLAlchemy URL
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todoapp.db"
+# SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{DB_PASS}@localhost:5432/TodoAppDB"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{DB_PASS}@127.0.0.1:3306/todoappdb"
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
